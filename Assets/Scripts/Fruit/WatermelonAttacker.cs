fb1587a2520b0be363437eff637cc6c00e378cb1
widewoods <widewooz@gmail.com>
2026-02-21 19:08:44 +0900

    Watermelon behavior

--- Assets/Scripts/Fruit/WatermelonAttacker.cs
+++ Assets/Scripts/Fruit/WatermelonAttacker.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WatermelonAttacker : MonoBehaviour
+public class WatermelonAttacker : MonoBehaviour, IAttacker
 {
-    // Start is called before the first frame update
+
+    private float attackTimer = 3f;
+    [SerializeField] private float attackCooldown = 3f;
+    [SerializeField] private float force = 5f;
+    private FruitController brain;
+
     void Start()
     {
-        
+        brain = GetComponent<FruitController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack(Transform target)
     {
-        
+        brain.enabled = false;
+        Vector3 direction = target.position - transform.position;
+        GetComponent<Rigidbody>().velocity = direction * force;
+    }
+
+    public bool CanAttack()
+    {
+        return true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground")) return;
+        brain.enabled = true;
     }
 }
